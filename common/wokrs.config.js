e239cab769ea819512d76465a30584ebe7539ec8
volvoldis <volvoldis@gmail.com>
2025-05-29 21:29:59 +0300

    update config

--- common/wokrs.config.js
+++ common/wokrs.config.js
@@ -3,33 +3,33 @@ import { SKILLS } from '@/common/constants';
 export const works = [
   // JOBS
   // IT HUB
-  {
-    href: 'https://ithub.vercel.app/',
-    title: 'IT Hub',
-    img: 'ithub.jpg',
-    type: 'Minimum Viable Product',
-    tools: [
-      SKILLS.express,
-      SKILLS.mongo,
-      SKILLS.react,
-      SKILLS.ts,
-      SKILLS.redux,
-      SKILLS.mantine,
-      SKILLS.webpack,
-      SKILLS.githubactions,
-    ],
-  },
+  // {
+  //   href: 'https://ithub.vercel.app/',
+  //   title: 'IT Hub',
+  //   img: 'ithub.jpg',
+  //   type: 'Minimum Viable Product',
+  //   tools: [
+  //     SKILLS.express,
+  //     SKILLS.mongo,
+  //     SKILLS.react,
+  //     SKILLS.ts,
+  //     SKILLS.redux,
+  //     SKILLS.mantine,
+  //     SKILLS.webpack,
+  //     SKILLS.githubactions,
+  //   ],
+  // },
   // Crypto Coins Compare
+  // {
+  //   href: 'https://coins-eosin.vercel.app/',
+  //   title: 'Crypto Coins Compare',
+  //   img: 'crypto.jpg',
+  //   type: 'Web application',
+  //   tools: [SKILLS.scss, SKILLS.next, SKILLS.antdesign, SKILLS.reactquery, SKILLS.githubactions],
+  // },
+  // // Seven Continents
   {
-    href: 'https://coins-eosin.vercel.app/',
-    title: 'Crypto Coins Compare',
-    img: 'crypto.jpg',
-    type: 'Web application',
-    tools: [SKILLS.scss, SKILLS.next, SKILLS.antdesign, SKILLS.reactquery, SKILLS.githubactions],
-  },
-  // Seven Continents
-  {
-    href: 'https://dmitriywolf.github.io/lp-seven-continent/dist/',
+    href: 'https://volvoldis.github.io/7-continents/dist/',
     title: 'Seven continents',
     img: 'lp/seven-continent.jpg',
     type: 'Landing page',
@@ -37,7 +37,7 @@ export const works = [
   },
   // Advo agency
   {
-    href: 'https://dmitriywolf.github.io/lp-advo-agency/dist/',
+    href: 'https://volvoldis.github.io/advo/dist/',
     title: 'Advo Agency',
     img: 'lp/advo-agency.jpg',
     type: 'Landing page',
@@ -45,7 +45,7 @@ export const works = [
   },
   // Yoga
   {
-    href: 'https://dmitriywolf.github.io/lp-yoga-tours/dist/',
+    href: 'https://volvoldis.github.io/yoga/dist/',
     title: 'Yoga tours to India',
     img: 'lp/yoga-tour.jpg',
     type: 'Landing page',
@@ -53,7 +53,7 @@ export const works = [
   },
   // Houses
   {
-    href: 'https://dmitriywolf.github.io/lp-houses-ages/dist/',
+    href: 'https://volvoldis.github.io/houses/dist/',
     title: 'Homes for Ages',
     img: 'lp/houses-ages.jpg',
     type: 'Landing page',
@@ -61,7 +61,7 @@ export const works = [
   },
   // 23 Degree
   {
-    href: 'https://dmitriywolf.github.io/lp-23degree/dist/',
+    href: 'https://volvoldis.github.io/23degree/dist/',
     title: '23 Degree',
     img: 'lp/degree.jpg',
     type: 'Landing page',
@@ -69,7 +69,7 @@ export const works = [
   },
   // Bouncy
   {
-    href: 'https://dmitriywolf.github.io/lp-bouncy/dist/',
+    href: 'https://volvoldis.github.io/bouncy/dist/',
     title: 'Bouncy',
     img: 'lp/bouncy.jpg',
     type: 'Landing page',
@@ -77,7 +77,7 @@ export const works = [
   },
   // Eddies cars
   {
-    href: 'https://dmitriywolf.github.io/lp-eddies/dist/',
+    href: 'https://volvoldis.github.io/belaz/dist/',
     title: "Eddie's-Car",
     img: 'lp/eddies.jpg',
     type: 'Landing page',
@@ -85,7 +85,7 @@ export const works = [
   },
   // Granit delivery
   {
-    href: 'https://dmitriywolf.github.io/lp-granit/dist/',
+    href: 'https://volvoldis.github.io/granit/dist/',
     title: 'Granit delivery',
     img: 'lp/granit.jpg',
     type: 'Landing page',
